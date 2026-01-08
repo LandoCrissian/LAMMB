5448d12d63272cd46ed8e8d2ca2931ef7dd43b91
Cronation <landon361@gmail.com>
2026-01-08 10:19:36 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+window.LAMMB = {
+  communityUrl: "https://x.com/i/communities/2009302380622565434",
+
+  launchMode: "prelaunch", // "prelaunch" | "live"
+
+  token: {
+    mint: "", // fill at launch
+    chain: "solana",
+    dexLinks: {
+      jupiter: "",
+      raydium: "",
+      pumpfun: "",
+      dexscreener: ""
+    }
+  }
+};
